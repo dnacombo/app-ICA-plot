74b3e0a5dea57aac4f5b98b891e2b4ab5a3db664
SaeedZahran <saeedzahran@hotmail.com>
2022-09-28 15:51:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 import mne
 import json
 import matplotlib.pyplot as plt
+
+#workaround for -- _tkinter.TclError: invalid command name ".!canvas"
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 # Load brainlife config.json
 with open('config.json','r') as config_f:
     config = json.load(config_f)
